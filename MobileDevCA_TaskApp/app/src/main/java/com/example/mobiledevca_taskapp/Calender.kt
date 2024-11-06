4e6681ed6b57d08e430fdcb92bdf7f4a66d4f2ed
Dylan <C22341463@mytudublin.ie>
2024-11-06 23:43:36 +0000

    Added a button that switches to calender view

--- MobileDevCA_TaskApp/app/src/main/java/com/example/mobiledevca_taskapp/Calender.kt
+++ MobileDevCA_TaskApp/app/src/main/java/com/example/mobiledevca_taskapp/Calender.kt
@@ -1,23 +1,15 @@
 package com.example.mobiledevca_taskapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Calender : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val CalenderBtn = findViewById<View>(R.id.CalenderBtn)
-        CalenderBtn.setOnClickListener{
-            val intent = Intent(this, Calender::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_calender)
     }
 }
