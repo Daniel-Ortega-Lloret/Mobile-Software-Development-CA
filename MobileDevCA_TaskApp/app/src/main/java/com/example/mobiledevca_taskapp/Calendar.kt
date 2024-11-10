bc7c5e4590a7dadc17cb29557f9acb7f9916b361
Daniel Ortega Lloret <C22726225@mytudublin.ie>
2024-11-10 15:00:41 +0000

    Added last activity and switching button

--- MobileDevCA_TaskApp/app/src/main/java/com/example/mobiledevca_taskapp/Calendar.kt
+++ MobileDevCA_TaskApp/app/src/main/java/com/example/mobiledevca_taskapp/Calendar.kt
@@ -2,23 +2,22 @@ package com.example.mobiledevca_taskapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class Calendar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_calendar)
 
-        _calendarBtn = findViewById<Button>(R.id.CalendarBtn)
-        _calendarBtn?.setOnClickListener{
-            val intent = Intent(this, Calendar::class.java)
+        _habitBtn = findViewById<Button>(R.id.habits_button)
+        _habitBtn?.setOnClickListener({
+            val intent = Intent(this, HabitsActivity::class.java)
             startActivity(intent)
-        }
+        })
     }
 
-    private var _calendarBtn: Button? = null
+    private var _habitBtn: Button? = null
 }
--- MobileDevCA_TaskApp/app/src/main/java/com/example/mobiledevca_taskapp/HabitsActivity.kt
+++ MobileDevCA_TaskApp/app/src/main/java/com/example/mobiledevca_taskapp/HabitsActivity.kt
@@ -2,23 +2,23 @@ package com.example.mobiledevca_taskapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class HabitsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_habit)
 
-        _calendarBtn = findViewById<Button>(R.id.CalendarBtn)
-        _calendarBtn?.setOnClickListener{
+        _habitCalendarBtn = findViewById<Button>(R.id.habits_calendarButton)
+
+        _habitCalendarBtn?.setOnClickListener({
             val intent = Intent(this, Calendar::class.java)
             startActivity(intent)
-        }
+        })
     }
 
-    private var _calendarBtn: Button? = null
+    private var _habitCalendarBtn: Button? = null;
 }
