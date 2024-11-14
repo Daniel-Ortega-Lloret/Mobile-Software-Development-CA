b5b73f03a3b208cb9b023126246d583b2c36da21
Daniel-Ortega-Lloret <C22726225@mytudublin.ie>
2024-11-14 00:51:14 +0000

    Added proper themes to proper file structure

--- app/src/main/java/com/example/mobiledevca_taskapp/common/BaseActivity.kt
+++ app/src/main/java/com/example/mobiledevca_taskapp/common/BaseActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -23,6 +24,7 @@ abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_base)
 
         binding = ActivityBaseBinding.inflate(layoutInflater)
