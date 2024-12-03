b4d3717e9aec07e3845f0259bea1fbb6feac98b3
Daniel-Ortega-Lloret <C22726225@mytudublin.ie>
2024-12-03 13:52:15 +0000

    Cleaned up the layouts

--- app/src/main/java/com/example/mobiledevca_taskapp/fragments/AddDataDialogFragment.kt
+++ app/src/main/java/com/example/mobiledevca_taskapp/fragments/AddDataDialogFragment.kt
@@ -5,6 +5,7 @@ import android.app.DatePickerDialog
 import android.app.Dialog
 import android.app.TimePickerDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
